205d1585c787ef559e558cf1ceac9656ca2d9e01
Pratik Patil <71940199+pratikpatil7777@users.noreply.github.com>
2022-06-24 00:28:27 -0400

    Frontend Hosting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import logo from "./logo.svg";
-import "./App.css";
+import "./index.css";
 import { TiWeatherSnow } from "react-icons/ti";
 import React, { useState } from "react";
 import { Helmet } from "react-helmet";
@@ -7,83 +7,107 @@ import axios from "axios";
 
 function App() {
   const [location, setLocation] = useState("");
-  const [result, setResult] = useState("");
+  const [data, setData] = useState("");
   async function postName(e) {
     e.preventDefault();
 
     try {
-      let currentWeather = await axios.post("http://localhost:4000/city", {
+      if (location === "") {
+        return;
+      }
+      let currWeather = await axios.post("http://localhost:4000/city", {
         location,
       });
-      console.log(currentWeather.data);
-      setResult(currentWeather.data);
+
+      console.log(currWeather.data);
+      setData(currWeather.data);
     } catch (error) {
       console.log(error);
     }
   }
   return (
-    <div className="App">
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>Weather App</title>
-        <link rel="canonical" href="http://tulsee.live" />
-        <meta name="description" content="Weather App" />
-      </Helmet>
-      <form onSubmit={postName}>
-        <input
-          type="text"
-          value={location}
-          placeholder="Enter City Name here..."
-          onChange={(event) => setLocation(event.target.value)}
-        />
-        <button type="submit"> Search </button>
-        {/* <{result ? { result } : ""}/>
-         */}
-      </form>
-      <div className="final">{result ? result : ""}</div>
-      {/* <div className="search">
-        <input
-          value={location}
-          onChange={(event) => setLocation(event.target.value)}
-          onKeyPress={searchLocation}
-          placeholder="Enter Location"
-          type="text"
-        />
-      </div>
-      <div className="container">
-        <div className="top">
-          <div className="location">
-            <p>{data.name}</p>
-          </div>
-          <div className="temp">
-            {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
-          </div>
-          <div className="description">
-            {data.weather ? <p>{data.weather[0].main}</p> : null}
-          </div>
-        </div>
-
-        {data.name !== undefined && (
-          <div className="bottom">
-            <div className="feels">
-              {data.main ? (
-                <p className="bold">{data.main.feels_like.toFixed()}°F</p>
-              ) : null}
-              <p>Feels Like</p>
-            </div>
-            <div className="humidity">
-              {data.main ? <p className="bold">{data.main.humidity}%</p> : null}
-              <p>Humidity</p>
-            </div>
-            <div className="wind">
-              {data.wind ? (
-                <p className="bold">{data.wind.speed.toFixed()} MPH</p>
-              ) : null}
-              <p>Wind Speed</p>
-            </div>
-          </div>
-        )}
+    <div>
+      {/* <div className="customNavbar">
+        <h1>Happy Weather</h1>
       </div> */}
+      <div className="app">
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>Weather App</title>
+          <link rel="canonical" href="http://tulsee.live" />
+          <meta name="description" content="Weather App" />
+        </Helmet>
+        <form onSubmit={postName}>
+          <h1 className="Heading">Happy Weather</h1>
+          <div className="component">
+            <input
+              className="search"
+              type="text"
+              style={{ boxShadow: "0px 2px 12px 10px rgba(0, 0, 0, 0.14)" }}
+              value={location}
+              placeholder="Enter City Name here..."
+              onChange={(event) => setLocation(event.target.value)}
+            />
+            <button type="submit" class="button-29" role="button">
+              Search
+            </button>
+
+            <button
+              class="button-29"
+              role="button"
+              onClick={() => setLocation(() => "")}
+            >
+              Clear
+            </button>
+          </div>
+          {/* {result ? { result } : ""} */}
+        </form>
+
+        <div className="container">
+          <div className="top">
+            <div className="location">
+              <p>{data.name}</p>
+            </div>
+            <div className="temp">
+              {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
+
+              {data.weather ? (
+                <p style={{ fontSize: "3em" }}>{data.weather[0].main}</p>
+              ) : null}
+            </div>
+          </div>
+
+          {data.name !== undefined && (
+            <div className="bottom">
+              <div
+                className="feels"
+                style={{ borderRight: "2px solid white", padding: "10px" }}
+              >
+                {data.main ? (
+                  <p className="bold">{data.main.feels_like.toFixed()}°F</p>
+                ) : null}
+                <p>Feels Like </p>
+              </div>
+
+              <div
+                className="humidity"
+                style={{ borderRight: "2px solid white", padding: "10px" }}
+              >
+                {data.main ? (
+                  <p className="bold">{data.main.humidity}%</p>
+                ) : null}
+                <p>Humidity</p>
+              </div>
+              <div className="wind" style={{ padding: "10px" }}>
+                {data.wind ? (
+                  <p className="bold">{data.wind.speed.toFixed()} MPH</p>
+                ) : null}
+                <p>Wind Speed</p>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
