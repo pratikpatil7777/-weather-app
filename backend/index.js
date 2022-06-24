5198eb5c8add8c7a92bccadedc7b530255139a32
Pratik Patil <71940199+pratikpatil7777@users.noreply.github.com>
2022-06-24 00:29:13 -0400

    Function modified

--- backend/index.js
+++ backend/index.js
@@ -12,11 +12,15 @@ app.get("/", cors(), async (req, res) => {
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ac7162a5301f68933e690486b0461860`,
     function (error, response, body) {
       let data = JSON.parse(body);
-      console.log(data.weather[0].description);
-      if (response.statusCode === 200) {
-        res.send(
-          `The weather in your city "${city}" is ${data.weather[0].description}`
-        );
+
+      try {
+        if (response.statusCode === 200) {
+          res.send(data);
+        } else {
+          console.log(response.statusCode);
+        }
+      } catch (e) {
+        response.send(e.message);
       }
     }
   );
@@ -30,14 +34,20 @@ app.post("/city", cors(), async (req, res) => {
     `https://api.openweathermap.org/data/2.5/weather?q=${city["location"]}&units=imperial&appid=ac7162a5301f68933e690486b0461860`,
     function (error, response, body) {
       let data = JSON.parse(body);
-      if (response.statusCode === 200) {
-        res.send(
-          data
-          //   `The weather in your city "${city["location"]}" is ${data.weather[0].description}`
-        );
+      console.log(data);
+      try {
+        if (response.statusCode === 200) {
+          res.send(data);
+        } else {
+          console.log(response.statusCode);
+        }
+      } catch (e) {
+        response.send(e.message);
       }
     }
   );
 });
 
-app.listen(4000, () => console.log("Server started on port 4000"));
+app.listen(process.env.PORT || 4000, () =>
+  console.log("Server started on port 4000")
+);
